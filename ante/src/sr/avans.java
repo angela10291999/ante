b858d1a5d0775dbbfffba7719d71a488999ab372
uno <uno@uno1608>
2017-12-13 15:34:01 -0500

    habla

---
+++ ante/src/sr/avans.java
@@ -0,0 +1,14 @@
+package sr;
+
+public class avans {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 0; i <= 12; i++)
+		{
+			System.out.print("12 * "+ i + " = " + 12 * i + "\n");
+		}
+
+	}
+
+}
